59733d554f729fda55833b4f2449a006d0ce464d
Camila Monreal <moncml@outlook.com>
2020-07-17 13:22:46 -0300

    readme

---
+++ build/precache-manifest.5c4774dc8c8c07d287f8a1ad2c17e18e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f2f74697bdc5582dcac7f9bf101009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "815d55af259cbe850955",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e57b13f8056538763975",
+    "url": "/static/js/2.e648d6a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e648d6a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "815d55af259cbe850955",
+    "url": "/static/js/main.f4a8ae59.chunk.js"
+  },
+  {
+    "revision": "68efef2dda077c4b2273",
+    "url": "/static/js/runtime-main.709f1246.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
