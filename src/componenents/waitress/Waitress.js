99e91b0fa2853f676c509427da434add81d6a057
sabia997 <sabi.sm12@gmail.com>
2020-07-26 19:14:10 -0300

    pagina do garçon H2

--- src/componenents/waitress/Waitress.js
+++ src/componenents/waitress/Waitress.js
@@ -1,10 +1,76 @@
-import React from "react";
-//import firebaseConfig from "../../firebase";
-
+import React, {useState} from "react";
+import {OptionsLanche, OptionsAcomp, OptionsBebidas, OptionsCafe}from './options'
+import {Mesa} from './mesa'
 const Waitress = () => {
+  const [showLanche, setShowLanche] = useState(false)
+  const [showAcomp, setShowAcomp] = useState(false)
+  const [showBebidas, setShowBebidas] = useState(false)
+  const [showCafe, setShowCafe] = useState(false)
+
+  const showOptionsLanche = () => {
+    if (showLanche === false) {
+      setShowLanche(true)
+    } else {
+      setShowLanche(false)
+    }
+  }
+  
+  const showOptionsAcomp = () => {
+    if (showAcomp === false) {
+      setShowAcomp(true)
+    } else {
+      setShowAcomp(false)
+    }
+  }
+  const showOptionsBebidas = () => {
+    if (showBebidas === false) {
+      setShowBebidas(true)
+    } else {
+      setShowBebidas(false)
+    }
+  }
+  const showOptionsCafe = () => {
+    if (showCafe === false) {
+      setShowCafe(true)
+    } else {
+      setShowCafe(false)
+    }
+  }
+
   return (
     <>
-      <h1>Waitress</h1>
+    <div className = 'garçon'>
+    <div className ='menu-pedidos'>
+      <div className = 'menu-opcao'>
+        <button onClick={showOptionsLanche} type = 'submit' className = 'input-pedido' >Lanches</button>
+        <img src = {require("./icones/hamburguer.png")} className ='icone' alt="ícone hamburguer"/>
+      </div>
+      { showLanche ? <OptionsLanche /> : null }
+
+      <div className = 'menu-opcao'>
+        <button onClick = {showOptionsAcomp} herf ='#box1' type = 'submit' className = 'input-pedido' >Acompanhamentos</button>
+        <img src = {require("./icones/batata.png")} className ='icone' alt="ícone batata"/>
+      </div>
+      { showAcomp ? <OptionsAcomp /> : null }
+
+       <div className = 'menu-opcao'>
+        <button onClick = {showOptionsBebidas} type = 'submit' className = 'input-pedido'> Bebidas</button>
+        <img src = {require("./icones/coca.png")} className ='icone' alt="ícone coca"/>
+      </div>
+        { showBebidas ? <OptionsBebidas /> : null }
+
+      <div className = 'menu-opcao'>
+        <button onClick = {showOptionsCafe} type = 'submit' className = 'input-pedido'>café da manhã</button>
+        <img src = {require("./icones/Coffee.png")}  className ='icone' alt="ícone café"/>
+      </div>
+      <div  >
+      { showCafe ? <OptionsCafe /> : null }
+        </div>
+    </div>
+    <div className = 'total-pedidos'>
+     < Mesa/> 
+    </div>
+    </div >
     </>
   );
 };
